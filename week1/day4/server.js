f4855b34c000243341c0ab7b8d87d4db09e300ed
inezajolly <inezajolly148@gmail.com>
2026-04-02 13:59:12 -0500

    Week 1: Completed SLMS (DB design, backend integration, JWT auth, borrow/return features, repo cleanup)

--- week1/day4/server.js
+++ week1/day4/server.js
@@ -20,3 +20,24 @@ app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
 
+// server.js
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// middleware
+app.use(bodyParser.json());
+
+// existing routes
+const borrowRoutes = require('./day4/routes/borrowRoutes');
+app.use('/api', borrowRoutes);
+
+// NEW Day 5 route
+const returnRoutes = require('./day5/routes/returnRoutes');
+app.use('/api', returnRoutes);
+
+// server start
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
